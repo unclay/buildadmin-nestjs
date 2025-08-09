2dede7f2efe4a81f28493ee22fd7d191fba27587
wcl <wcl58981298@gmail.com>
2025-08-09 10:08:57 +0800

    fix(auth): remove request-context

--- basys/src/modules/auth/request-context.service.ts
+++
@@ -1,8 +0,0 @@
-import { Inject, Injectable, Scope } from '@nestjs/common';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
-
-@Injectable({ scope: Scope.REQUEST })
-export class RequestContext {
-  constructor(@Inject(REQUEST) public readonly request: Request) {}
-}
